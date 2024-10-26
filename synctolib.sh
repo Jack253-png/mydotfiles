6761e263659187cfd5bddc902ad0d9fef4e787c2
Jack253-png <guoxiuchen20170402@163.com>
2024-10-27 06:58:24 +0800

    Initial Commit

---
+++ synctolib.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cp -r ~/.config/alacritty ./alacritty
+cp -r ~/.config/waybar ./waybar
+cp -r ~/.config/hypr ./hypr
+cp -r ~/.config/nvim ./nvim
---
+++ synctolocal.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+rm -r ~/.config/hypr && cp -r ./hypr/* ~/.config
+rm -r ~/.config/waybar && cp -r ./waybar/* ~/.config
+rm -r ~/.config/alacritty && cp -r ./alacritty/* ~/.config
+rm -r ~/.config/nvim && cp -r ./nvim/* ~/.config
