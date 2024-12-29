8a517963666552f3b3c559055fd595e2cc2d3493
Jack253-png <guoxiuchen20170402@163.com>
2024-12-29 11:42:35 +0800

    Update

--- synctolib.sh
+++ synctolib.sh
@@ -4,3 +4,4 @@ cp -r ~/.config/alacritty ./alacritty
 cp -r ~/.config/waybar ./waybar
 cp -r ~/.config/hypr ./hypr
 cp -r ~/.config/nvim ./nvim
+cp -r ~/.config/kitty ./kitty
--- synctolocal.sh
+++ synctolocal.sh
@@ -4,3 +4,4 @@ rm -r ~/.config/hypr && cp -r ./hypr/* ~/.config
 rm -r ~/.config/waybar && cp -r ./waybar/* ~/.config
 rm -r ~/.config/alacritty && cp -r ./alacritty/* ~/.config
 rm -r ~/.config/nvim && cp -r ./nvim/* ~/.config
+rm -r ~/.config/kitty && cp -r ./kitty/* ~/.config
